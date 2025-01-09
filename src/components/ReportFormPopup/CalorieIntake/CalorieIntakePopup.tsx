09de2830de84a94366e30f170008b662734db2d4
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-01-09 12:35:38 +0530

    Day-3-4

--- src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
+++ src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
@@ -4,11 +4,17 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import DatePicker from "react-horizontal-datepicker";
 import { AiFillDelete, AiOutlineClose } from 'react-icons/ai'
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimeClock } from '@mui/x-date-pickers/TimeClock';
 
 interface calorieintakepopupprops{
   setShowCalorieIntakePopup:React.Dispatch<React.SetStateAction<boolean>>
 }
 const CalorieIntakePopup:React.FC<calorieintakepopupprops> = ({setShowCalorieIntakePopup}) => {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
   const color = '#ffc20e'
   const [date,setDate]=React.useState<any>(new Date())
   const selectedDay = (val: any) => {
@@ -30,6 +36,33 @@ const CalorieIntakePopup:React.FC<calorieintakepopupprops> = ({setShowCalorieInt
           labelFormat={"MMMM"}
           color={color}
         />
+        <TextField id="outlined-basic" label="Food item name" variant="outlined" color="warning"/>
+        <TextField id="outlined-basic" label="Food item amount (in gms)" variant="outlined" color="warning" />
+        <div className='timebox'>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <TimeClock value={value} onChange={(newValue) => setValue(newValue)} />
+          </LocalizationProvider>
+          </div>
+          <Button variant='contained' color='warning'> save</Button>
+          <div className='hrline'></div>
+        <div className='items'>
+          <div className='item'>
+            <h3>Apple</h3>
+            <h3>100 gms</h3>
+            <button> <AiFillDelete /></button>
+          </div>
+          <div className='item'>
+            <h3>Banana</h3>
+            <h3>200 gms</h3>
+            <button> <AiFillDelete /></button>
+
+          </div>
+          <div className='item'>
+            <h3>Rice</h3>
+            <h3>300 gms</h3>
+            <button> <AiFillDelete /></button>
+            </div>
+        </div>
      </div>
       </div>
   )
