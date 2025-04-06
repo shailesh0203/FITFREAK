acc700a2cf42d8fc63ef0720bf0351981ff7d545
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-04-06 22:30:02 +0530

    ll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,28 +5,61 @@ import { IoIosBody } from 'react-icons/io'
 import './Navbar.css'
 import Image from 'next/image'
 import Link from 'next/link'
-import Authpopup from '../Authpopup/Authpopup'
+import AuthPopup from '../Authpopup/Authpopup'
 const Navbar = () => {
-  const [isloggedin,setIsloggedin]=React.useState<boolean>(false)
-  const[showpopup,setShowpopup]=React.useState<boolean>(false);
-  return (
-    <nav>
-        <Image src={logo} alt="Logo"/>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/profile'><IoIosBody/></Link>
-        {
-        isloggedin?
-      <button>Logout</button>:
-      <button
-      onClick={()=>setShowpopup(true)}
-      >Login</button>
-      }
-      {
-        showpopup && <Authpopup setShowpopup={setShowpopup}/>
-      }
-    </nav>
-  )
+    const [isloggedin, setIsloggedin] = React.useState<boolean>(false)
+
+    const [showpopup, setShowpopup] = React.useState<boolean>(false)
+    const checklogin = async () => {
+        fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/auth/checklogin', {
+            method: 'POST',
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.ok) {
+                    setIsloggedin(true)
+                }
+                else{
+                    setIsloggedin(false)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+
+    React.useEffect(() => {
+        checklogin()
+    }, [showpopup])
+
+    return (
+        <nav>
+            <Image src={logo} alt="Logo" />
+            <Link href='/'>Home</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/profile'><IoIosBody /></Link>
+            {
+                isloggedin ?
+                    <button>Logout</button>
+                    :
+                    <button
+                        onClick={() => {
+                            setShowpopup(true)
+                        }}
+                    >Login</button>
+
+
+
+            }
+
+            {
+                showpopup && <AuthPopup setShowpopup={setShowpopup} />
+            }
+        </nav>
+    )
 }
 
 export default Navbar
