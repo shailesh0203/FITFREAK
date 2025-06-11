78590b672591cd8a572690bfbf127ca8ef314e78
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-06-11 22:31:01 +0530

    ..

--- src/app/report/[type]/page.tsx
+++ src/app/report/[type]/page.tsx
@@ -5,59 +5,38 @@ import './ReportPage.css'
 import {LineChart} from '@mui/x-charts/LineChart'
 import {AiFillEdit} from 'react-icons/ai'
 import CalorieIntakePopup from '@/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup'
+import { usePathname } from 'next/navigation'
 
 const page = () => {
    
     const color='#ffc20e'
+    const pathname=usePathname()
     const chartsParams = {
         // margin: { bottom: 20, left: 25, right: 5 },
         height: 300,
     };
     const[dataS1,setDataS1]=React.useState<any>(null)
     const getDataForS1=async()=>{
-        let temp = [
-            {
-                date: 'Thu Sep 28 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2000,
+        if(pathname=='/report/Calorie%20Intake'){
+            fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/calorieintake/getcalorieintakebylimit', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ limit: 10 })
+})
+.then(res=>res.json())
+.then(data => {
+    if (data.ok) {
+        let temp = data.data.map((item: any) => {
+            return {
+                date: item.date,
+                value: item.calorieIntake,
                 unit: 'kcal'
-            },
-            {
-                date: 'Wed Sep 27 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2500,
-                unit: 'kcal'
-            },
-            {
-                date: 'Tue Sep 26 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2700,
-                unit: 'kcal'
-            },
-            {
-                date: 'Mon Sep 25 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 3000,
-                unit: 'kcal'
-            },
-            {
-                date: 'Sun Sep 24 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2000,
-                unit: 'kcal'
-            },
-            {
-                date: 'Sat Sep 23 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2300,
-                unit: 'kcal'
-            },
-            {
-                date: 'Fri Sep 22 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2500,
-                unit: 'kcal'
-            },
-            {
-                date: 'Thu Sep 21 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2700,
-                unit: 'kcal'
-            },
-        ]
-        let dataForLineChart=temp.map((item:any)=>{
+            }
+        })
+         let dataForLineChart=temp.map((item:any)=>{
             let val=JSON.stringify(item.value)
             return val
         })
@@ -66,8 +45,7 @@ const page = () => {
             let val=(new Date(item.date))
             return val
         })
-        
-        setDataS1({
+         setDataS1({
             data:dataForLineChart,
             title:'1 Day calorie intake',
             color:color,
@@ -78,12 +56,70 @@ const page = () => {
             }
         })
     }
+    else {
+        setDataS1([])
+    }
+})
+.catch(err=>{
+    console.log(err)
+})
+
+        }else{
+            alert('get data for other reports')
+        }
+        // let temp = [
+        //     {
+        //         date: 'Thu Sep 28 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2000,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Wed Sep 27 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2500,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Tue Sep 26 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2700,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Mon Sep 25 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 3000,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Sun Sep 24 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2000,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Sat Sep 23 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2300,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Fri Sep 22 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2500,
+        //         unit: 'kcal'
+        //     },
+        //     {
+        //         date: 'Thu Sep 21 2023 20:30:30 GMT+0530 (India Standard Time)',
+        //         value: 2700,
+        //         unit: 'kcal'
+        //     },
+        // ]
+       
+        
+       
+    }
     React.useEffect(()=>{
         getDataForS1()
     },[])
    const [showCalorieIntakePopup,setShowCalorieIntakePopup] =React.useState<boolean>(false)
   return (
     <div className='reportpage'>
+    {   dataS1?.length>0 &&
         <div className='s1'>
         { dataS1 &&
          <LineChart
@@ -106,7 +142,9 @@ const page = () => {
      {...chartsParams}
     />
 }
+
 </div>
+}
 
 <button className='editbutton'
 onClick={()=>{
